0ed445e61c3e54a120c6e75c4a07f136cf006708
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2025-09-23 13:10:45 +0530

    initial frontend with tailwind & backend setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import app from "./app.js";
+
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Server is running")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
